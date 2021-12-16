15b0843d55b04616e09e58a32589b2b1086c1c97
dai316 <179296165@qq.com>
2021-12-16 08:49:46 +0800

    Search

---
+++ src/components/MusicItem.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <van-cell-group>
+    <van-cell
+      center
+      :title="name"
+      :label="singerName+(albumName !== undefined ?'-'+albumName:'')"
+    >
+      <template v-slot:right-icon>
+        <van-icon name="play-circle-o" size="0.6rem"/>
+      </template>
+    </van-cell>
+  </van-cell-group>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'MusicItem',
+  props: {
+    name: String, // 歌名
+    id: Number, // ID
+    albumName: String, // 专辑名
+    singerName: String // 歌手名
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
