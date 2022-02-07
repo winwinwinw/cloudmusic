fea4bcb97b2d5dd00008ccf642a02375b21f48ec
dai316 <179296165@qq.com>
2022-02-07 12:33:01 +0800

    d

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,7 @@ function resolve (dir) {
   return path.join(__dirname, dir)
 }
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/cloudmusic/'
-    : './',
+  publicPath: './',
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
